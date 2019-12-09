30970147fe4a0cd896e7e92f69fbba9ba3d5503a
afrouz <afrouzhakim@gmail.com>
2019-12-09 17:23:02 +0100

    add more details for products

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -1,3 +1,36 @@
-console.log('Script loaded');
+/*const productNames = getAvailableProducts();
+function renderProducts (productNames) {
+    let ulvalue = document.getElementById("test")
+    for (let i = 0; i< productNames.length; i++) {
+        const Ele = document.createElement ("LI");
+        Ele.innerHTML = `<li style="circle"> ${productNames[i].name} </li>`;
+        ulvalue.appendChild(Ele);
+    }
+}
+renderProducts (productNames);
+*/
 
-console.log(getAvailableProducts());
+const products = getAvailableProducts();
+const myList = document.getElementById("test");
+function prepareProduct(product) {
+  const li = document.createElement("li");
+  const propertiesList = document.createElement("ul");
+  const idLi = document.createElement("li");
+  idLi.textContent = product.id;
+  propertiesList.appendChild(idLi);
+  const nameLi = document.createElement("li");
+  nameLi.textContent = product.name;
+  propertiesList.appendChild(nameLi);
+  /*   const countriesList = document.createElement("ul");
+  const countryItem = document.createElement("li");
+  for (let i = 0; i < product.shipsTo.length; i++) {
+    const country = product.shipsTo[i];
+  }
+ */
+  li.appendChild(propertiesList);
+  myList.appendChild(li);
+}
+for (let i = 0; i < products.length; ++i) {
+  const product = products[i];
+  prepareProduct(product);
+}
---
+++ javascript/javascript2/week2/script.js
@@ -0,0 +1,205 @@
+const listings = [
+    {
+      "type": "Shed",
+      "facilities": [
+        "Parkering"
+      ],
+      "price": 1311,
+      "hasGarden": true,
+      "size": 383
+    },
+    {
+      "type": "House",
+      "facilities": [
+        "Elevator",
+        "Parkering"
+      ],
+      "price": 752,
+      "hasGarden": true,
+      "size": 627
+    },
+    {
+      "type": "Shed",
+      "facilities": [
+        "Have"
+      ],
+      "price": 2564,
+      "hasGarden": false,
+      "size": 349
+    },
+    {
+      "type": "Shed",
+      "facilities": [
+        "Parkering",
+        "Altan"
+      ],
+      "price": 2309,
+      "hasGarden": true,
+      "size": 687
+    },
+    {
+      "type": "Shed",
+      "facilities": [
+        "Have"
+      ],
+      "price": 1269,
+      "hasGarden": false,
+      "size": 76
+    },
+    {
+      "type": "Apartment",
+      "facilities": [
+        "Elevator",
+        "Have"
+      ],
+      "price": 9032,
+      "hasGarden": true,
+      "size": 25
+    },
+    {
+      "type": "Farm",
+      "facilities": [
+        "Altan",
+        "Parkering"
+      ],
+      "price": 8266,
+      "hasGarden": true,
+      "size": 697
+    },
+    {
+      "type": "House",
+      "facilities": [
+        "Husdyr",
+        "Elevator"
+      ],
+      "price": 8601,
+      "hasGarden": true,
+      "size": 573
+    },
+    {
+      "type": "House",
+      "facilities": [
+        "Husdyr"
+      ],
+      "price": 3041,
+      "hasGarden": true,
+      "size": 872
+    },
+    {
+      "type": "House",
+      "facilities": [
+        "Parkering"
+      ],
+      "price": 8233,
+      "hasGarden": true,
+      "size": 781
+    },
+    {
+      "type": "Shed",
+      "facilities": [
+        "Altan",
+        "Husdyr"
+      ],
+      "price": 9842,
+      "hasGarden": false,
+      "size": 280
+    },
+    {
+      "type": "House",
+      "facilities": [
+        "Have",
+        "Altan"
+      ],
+      "price": 5387,
+      "hasGarden": false,
+      "size": 338
+    },
+    {
+      "type": "Apartment",
+      "facilities": [
+        "Have",
+        "Husdyr"
+      ],
+      "price": 4233,
+      "hasGarden": false,
+      "size": 252
+    },
+    {
+      "type": "Farm",
+      "facilities": [
+        "Altan",
+        "Have",
+        "Parkering"
+      ],
+      "price": 8477,
+      "hasGarden": true,
+      "size": 917
+    },
+    {
+      "type": "Farm",
+      "facilities": [
+        "Have",
+        "Altan",
+        "Husdyr",
+        "Elevator"
+      ],
+      "price": 6904,
+      "hasGarden": true,
+      "size": 339
+    },
+    {
+      "type": "Farm",
+      "facilities": [
+        "Husdyr",
+        "Have"
+      ],
+      "price": 5601,
+      "hasGarden": true,
+      "size": 474
+    },
+    {
+      "type": "Apartment",
+      "facilities": [
+        "Altan",
+        "Husdyr"
+      ],
+      "price": 790,
+      "hasGarden": true,
+      "size": 896
+    },
+    {
+      "type": "Farm",
+      "facilities": [
+        "Husdyr",
+        "Altan"
+      ],
+      "price": 543,
+      "hasGarden": false,
+      "size": 984
+    },
+    {
+      "type": "Dorm",
+      "facilities": [
+        "Husdyr",
+        "Elevator"
+      ],
+      "price": 7382,
+      "hasGarden": false,
+      "size": 95
+    },
+    {
+      "type": "Farm",
+      "facilities": [
+        "Elevator",
+        "Altan",
+        "Husdyr"
+      ],
+      "price": 9280,
+      "hasGarden": false,
+      "size": 597
+    }
+  ]
+
+  listings.forEach(function(x) {
+    console.log (x);
+  })
