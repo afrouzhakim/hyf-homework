d30c398f6e5b250403a8a9f94f1e27f70948c4e9
afrouz <afrouzhakim@gmail.com>
2020-06-05 18:56:36 +0200

    change the function to return correct string

--- javascript/javascript1/week2/weather wear/wear.js
+++ javascript/javascript1/week2/weather wear/wear.js
@@ -1,14 +1,14 @@
 function wearWhat(degree) {
     if (degree<-30) {
-        console.log("You will be die anyway!");
+        return "You will be die anyway!";
     }else if (-30<degree<=0) {
-        console.log("You must to wear winter jacket");
+        return "You must to wear winter jacket";
     }else if (0<degree<=10) {
-        console.log("You must to wear normal jacket");
+        return "You must to wear normal jacket";
     }else if (10<degree<=30) {
-        console.log("You must to wear pants");
+        return "You must to wear pants";
     }else if (30<degree) {
-        console.log("You must to wear shorts");
+        return "You must to wear shorts";
     }
 }
-wearWhat(20);
+alert(wearWhat(20));
