85eeb56a813b677eb38af885c0b5b0059ec3e7d0
afrouz <afrouzhakim@gmail.com>
2020-06-06 12:33:54 +0200

    replacing for loop by array.includes()

--- javascript/javascript1/week3/item/item.js
+++ javascript/javascript1/week3/item/item.js
@@ -1,9 +1,7 @@
 const names = ['Peter', 'Ahmad', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala'];
 const nameToRemove = 'Ahmad';
-console.log(names);
-for(i=0; i<names.length; i++) {
-    if ( names [i] === nameToRemove ) {
-        names.splice(i, 1); 
-      }
+
+if (names.includes(nameToRemove)) {
+  names.splice(names.indexOf(nameToRemove), 1);
 }
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
