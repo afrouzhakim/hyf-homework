6c91b4fab394d068f24c9aa1a09f2dd630434a85
afrouz <afrouzhakim@gmail.com>
2020-06-07 10:54:08 +0200

    added logOutNotesFormatted function

--- javascript/javascript1/week3/note/note.js
+++ javascript/javascript1/week3/note/note.js
@@ -1,26 +1,14 @@
-let notes = [];
-
-function addNote(myContent, myId) {
-  notes.push({'content': myContent, 'id': myId});
-}
-
-function getNoteFromId(id) {
-  for (let i = 0; i < notes.length; i++) {
-    if (id === notes[i].id) {
-      return notes[i].content;
-    } else {
-      console.log("There is no match id.");
-    }
-  }
-}
-
-function getAllNotes() {
-  return notes;
+//save not
+const notes = [];
+function saveNote(content, id) {
+  obj = {content: 'Pick up groceries', id: 1}, {content: 'Do laundry', id: 2}
+  notes.push(obj);
 }
 
 function logOutNotesFormatted() {
   for (let i = 0; i < notes.length; i++) {
-    console.log(`The note with id: ${notes[i].id}, has the following note text: ${notes[i].content}.`);
+  console.log(`The note with id: ${notes[i].id}, has the following text: ${notes[i].content}`)
   }
 }
 
+logOutNotesFormatted();
