6d5f99e4925fcdb97a5725277a419fad42a8062f
afrouz <afrouzhakim@gmail.com>
2020-06-09 17:29:59 +0200

    updated the note function

--- javascript/javascript1/week3/note/note.js
+++ javascript/javascript1/week3/note/note.js
@@ -1,14 +1,29 @@
+//we are writing a note saving app with the ability to add and save some notes, retrieve a note by giving note id, read all the notes,
 //save not
 const notes = [];
 function saveNote(content, id) {
-  obj = {content: 'Pick up groceries', id: 1}, {content: 'Do laundry', id: 2}
+  obj = {content:content,
+         id: id}
   notes.push(obj);
 }
+ saveNote('Pick up groceries', 1);
+ saveNote('Do laundry', 2);
+console.log(notes); 
 
+//Search a note by id
+function getNoteById(id) {
+  const content = notes.filter(item => item.id===id)
+  if (content.length < 1) {
+    return "The id is not found."
+  } 
+    return content;
+}
+console.log(getNoteById(2));
+
+//read all the notes
 function logOutNotesFormatted() {
   for (let i = 0; i < notes.length; i++) {
   console.log(`The note with id: ${notes[i].id}, has the following text: ${notes[i].content}`)
   }
 }
-
 logOutNotesFormatted();
