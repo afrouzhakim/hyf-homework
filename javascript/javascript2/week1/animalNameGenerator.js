3285589c5500c6290de96ef8155a1c115b675277
afrouz <afrouzhakim@gmail.com>
2020-06-18 20:26:47 +0200

    edit animalNameGeneration by adding addEventListener()

--- javascript/javascript2/week1/animalNameGenerator.js
+++ javascript/javascript2/week1/animalNameGenerator.js
@@ -1,7 +1,41 @@
-const spiritAnimalString = ["spiritAnimal1", "spiritAnimal2", "spiritAnimal3", "spiritAnimal4", "spiritAnimal5", "spiritAnimal6", "spiritAnimal7", "spiritAnimal8", "spiritAnimal9", "spiritAnimal10"];
-function getusername () {
-    let username = document.getElementById ("username").value;
-    let random = Math.round(Math.random()*10);
-    console.log (random);
-    alert(`${username} - ${spiritAnimalString[random]}`)
-}
+let name;
+const spiritAnimal = ["spiritAnimal1", "spiritAnimal2", "spiritAnimal3", "spiritAnimal4", "spiritAnimal5", "spiritAnimal6", "spiritAnimal7", "spiritAnimal8", "spiritAnimal9", "spiritAnimal10"];
+
+function button() {
+    if (document.getElementById("event").value === "1") {
+      combine();
+    }
+  }
+  
+  function hover() {
+    if (document.getElementById("event").value === "2") {
+      combine();
+    }
+  }
+  
+  function input() {
+    myName();
+    if (document.getElementById("event").value === "3") {
+      combine();
+    }
+  }
+  
+  function combine() {
+    if (name) {
+      let spiritName = spiritAnimal[Math.floor(Math.random() * spiritAnimal.length)];
+      document.getElementById("spirit-name").innerHTML = `${name} - ${spiritName}`;
+    }
+    else {
+      document.getElementById("spirit-name").innerHTML = "Please enter your name first";
+    }
+  }
+  
+  let userName = document.getElementById("name");
+  userName.addEventListener("click", button);
+  userName.addEventListener("mouseover", hover);
+  userName.addEventListener("input", input);
+  
+  function myName() {
+    name = document.getElementById("name").value;
+  }
+
